436e1dc408d825a0477ad45e98e36c8275729112
Umang-Shroff <143543280+Umang-Shroff@users.noreply.github.com>
2024-09-27 18:56:18 +0530

    Update modifyPdf.js

--- src/components/modifyPdf/modifyPdf.js
+++ src/components/modifyPdf/modifyPdf.js
@@ -1,29 +1,122 @@
-import React from 'react';
-import { useState } from 'react';
+// import React from 'react';
+// import { useState } from 'react';
+// import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+// // import fontkit from '@pdf-lib/fontkit';
+// import './modifyPdf.css'
+
+// const ModifyPdf = ({ pdfBytes }) => {
+
+//     const [xdist, setXdist] = useState(0);
+//     const [ydist, setYdist] = useState(0);
+//     const [textChange, setTextChange] = useState('');
+//     const [fontChange, setFontChange] = useState(0);
+
+//   const handleModifyPdf = async () => {
+//     const pdfDoc = await PDFDocument.load(pdfBytes);
+    // const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+//     const pages = pdfDoc.getPages();
+//     const firstPage = pages[0];
+//     const { width, height } = firstPage.getSize();
+
+    // const text = textChange;
+    // const fontSize = parseInt(fontChange);
+
+    // const textWidth = helveticaFont.widthOfTextAtSize(text, fontSize);
+    // const textHeight = fontSize; 
+
+    // firstPage.drawRectangle({
+    //   x: parseInt(xdist),   //x: 450
+    //   y: 770-parseInt(ydist),   //y: 660
+    //   width: textWidth+5,
+    //   height: textHeight + 5, 
+    //   color: rgb(1, 1, 1), 
+    //   // borderColor: rgb(0, 0, 0), 
+    //   // borderWidth: 1,
+    // });
+
+    // const timesNewRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+    // firstPage.drawText(text, {
+    //   x: parseInt(xdist)+5,     //x:450 + 5
+    //   y: 770-parseInt(ydist)+5,     //y:660 + 5
+    //   size: fontSize,
+    //   font: timesNewRomanFont,
+    //   color: rgb(0,0,0),
+    // });
+
+    // firstPage.drawText(text, {
+    //   x: parseInt(xdist)+5.5,     //x:450 + 5
+    //   y: 770-parseInt(ydist)+5,     //y:660 + 5
+    //   size: fontSize,
+    //   font: timesNewRomanFont,
+    //   color: rgb(0,0,0),
+    // });
+    
+//     const modifiedPdfBytes = await pdfDoc.save();
+
+//     const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+//     const urlBlob = URL.createObjectURL(blob);
+
+//     const link = document.createElement('a');
+//     link.href = urlBlob;
+//     link.download = 'modified_pdf.pdf';
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//     URL.revokeObjectURL(urlBlob); 
+//     setTextChange('');
+//     setXdist(0);
+//     setYdist(0);
+//     setFontChange(0);
+//   };
+
+//   return (
+//     <div className="inp-div">
+//       <h2 className="input-head">Enter values for Text overlay</h2>
+//       <h4>Enter Text to be added: <input className="inps" value={textChange} type='text' onChange={(e)=>setTextChange(e.target.value)}/></h4>
+//       <h4>Enter x: <input className="inps" type='number' value={xdist} onChange={(e)=>setXdist(e.target.value)} /></h4>
+//       <h4>Enter y: <input className="inps" type='number' value={ydist} onChange={(e)=>setYdist(e.target.value)}/></h4>
+//       <p>For eg: x = 300 , y = 100 for Top-Center</p>
+//       <h4>Enter Font size: <input value={fontChange} className="inps" type='number' onChange={(e)=>setFontChange(e.target.value)}/></h4>
+//       <span className="span-note">By default font : Times New Roman Bold</span>
+//       <button className='modify-btn' onClick={handleModifyPdf}>UPDATE PDF</button>
+//     </div>
+//   );
+// };
+
+// export default ModifyPdf;
+
+
+
+import React, { useState } from 'react';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-// import fontkit from '@pdf-lib/fontkit';
 import './modifyPdf.css'
+import ModifyPdfViewer from '../modifyPdfViewer/modifyPdfViewer';
 
 const ModifyPdf = ({ pdfBytes }) => {
-
-    const [xdist, setXdist] = useState(0);
-    const [ydist, setYdist] = useState(0);
-    const [textChange, setTextChange] = useState('');
-    const [fontChange, setFontChange] = useState(0);
+  const [xdist, setXdist] = useState(0);
+  const [ydist, setYdist] = useState(0);
+  const [textChange, setTextChange] = useState('');
+  const [fontChange, setFontChange] = useState(0);
+  const [modifiedPdfBytes, setModifiedPdfBytes] = useState(null);
 
   const handleModifyPdf = async () => {
     const pdfDoc = await PDFDocument.load(pdfBytes);
-    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-    const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const { width, height } = firstPage.getSize();
 
     const text = textChange;
     const fontSize = parseInt(fontChange);
 
-    const textWidth = helveticaFont.widthOfTextAtSize(text, fontSize);
-    const textHeight = fontSize; 
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const textWidth = helveticaFont.widthOfTextAtSize(text, parseInt(fontSize));
+    const textHeight = parseInt(fontSize); 
+
+    
+
+
+    const timesNewRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+    const firstPage = pdfDoc.getPages()[0];
 
     firstPage.drawRectangle({
       x: parseInt(xdist),   //x: 450
@@ -35,56 +128,86 @@ const ModifyPdf = ({ pdfBytes }) => {
       // borderWidth: 1,
     });
 
-    const timesNewRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-    firstPage.drawText(text, {
+    firstPage.drawText(textChange, {
       x: parseInt(xdist)+5,     //x:450 + 5
       y: 770-parseInt(ydist)+5,     //y:660 + 5
-      size: fontSize,
+      size: parseInt(fontChange),
       font: timesNewRomanFont,
       color: rgb(0,0,0),
     });
 
-    firstPage.drawText(text, {
+    firstPage.drawText(textChange, {
       x: parseInt(xdist)+5.5,     //x:450 + 5
       y: 770-parseInt(ydist)+5,     //y:660 + 5
-      size: fontSize,
+      size: parseInt(fontChange),
       font: timesNewRomanFont,
       color: rgb(0,0,0),
     });
-    
+
+    // firstPage.drawText(textChange, {
+    //   x: parseInt(xdist),
+    //   y: firstPage.getHeight() - parseInt(ydist),
+    //   size: parseInt(fontChange),
+    //   font: timesNewRomanFont,
+    //   color: rgb(0, 0, 0),
+    // });
+
     const modifiedPdfBytes = await pdfDoc.save();
+    setModifiedPdfBytes(modifiedPdfBytes);
 
-    const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
-    const urlBlob = URL.createObjectURL(blob);
-
-    const link = document.createElement('a');
-    link.href = urlBlob;
-    link.download = 'modified_pdf.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(urlBlob); 
+    // Reset input fields
     setTextChange('');
     setXdist(0);
     setYdist(0);
     setFontChange(0);
   };
 
+  const handleDownloadPdf = () => {
+    if (modifiedPdfBytes) {
+      const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+      const urlBlob = URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = urlBlob;
+      link.download = 'modified_pdf.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(urlBlob);
+    }
+  };
+
   return (
     <div className="inp-div">
       <h2 className="input-head">Enter values for Text overlay</h2>
-      <h4>Enter Text to be added: <input className="inps" value={textChange} type='text' onChange={(e)=>setTextChange(e.target.value)}/></h4>
-      <h4>Enter x: <input className="inps" type='number' value={xdist} onChange={(e)=>setXdist(e.target.value)} /></h4>
-      <h4>Enter y: <input className="inps" type='number' value={ydist} onChange={(e)=>setYdist(e.target.value)}/></h4>
-      <p>For eg: x = 300 , y = 100 for Top-Center</p>
-      <h4>Enter Font size: <input value={fontChange} className="inps" type='number' onChange={(e)=>setFontChange(e.target.value)}/></h4>
-      <span className="span-note">By default font : Times New Roman Bold</span>
+      <h4>
+        Enter Text to be added:{' '}
+        <input className="inps" value={textChange} type='text' onChange={(e) => setTextChange(e.target.value)} />
+      </h4>
+      <h4>
+        Enter x: <input className="inps" type='number' value={xdist} onChange={(e) => setXdist(e.target.value)} />
+      </h4>
+      <h4>
+        Enter y: <input className="inps" type='number' value={ydist} onChange={(e) => setYdist(e.target.value)} />
+      </h4>
+      <h4>
+        Enter Font size: <input value={fontChange} className="inps" type='number' onChange={(e) => setFontChange(e.target.value)} />
+      </h4>
       <button className='modify-btn' onClick={handleModifyPdf}>UPDATE PDF</button>
+      
+      {modifiedPdfBytes && (
+        <>
+          <ModifyPdfViewer modifiedPdfBytes={modifiedPdfBytes} />
+          <button className='download-btn' onClick={handleDownloadPdf}>Download PDF</button>
+        </>
+      )}
     </div>
   );
 };
 
 export default ModifyPdf;
+
+
 
 
 
